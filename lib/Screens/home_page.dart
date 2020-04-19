2e18795f5615649a550f4e337a0dc8ae4f03b355
Benson <bensonpx1198@gmail.com>
2020-04-19 12:18:34 +0530

    first-commit

---
+++ lib/Screens/home_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:stocktracker/reusable_card.dart';
+import 'package:stocktracker/constants.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.blue[900],
+              Colors.blue[700],
+              Colors.blue[500],
+            ],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 80, bottom: 80),
+              child: Text(
+                'Welcome',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60.0),
+                      topRight: Radius.circular(60.0)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ReusableCard(
+                      colour: Color(0xFF2B2929),
+                      cardChild: Padding(
+                        padding: EdgeInsets.all(30),
+                        child: Text(
+                          'View Stocks',
+                          style: kHomeButton,
+                        ),
+                      ),
+                    ),
+                    ReusableCard(
+                      colour: Color(0xFF2B2929),
+                      cardChild: Padding(
+                        padding: EdgeInsets.all(30),
+                        child: Text(
+                          'Update Stocks',
+                          style: kHomeButton,
+                        ),
+                      ),
+                    ),
+                    ReusableCard(
+                      colour: Color(0xFF2B2929),
+                      cardChild: Padding(
+                        padding: EdgeInsets.all(30),
+                        child: Text(
+                          'Add a Book',
+                          style: kHomeButton,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kHomeButton =
+    TextStyle(fontSize: 25.0, fontWeight: FontWeight.w700, color: Colors.white);
